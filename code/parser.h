5acc47240b0aa222af1caebd1e8bb03559721599
pedrops164 <A95826@alunos.uminho.pt>
2021-03-30 18:09:04 +0100

    documentacao pushpop.h e parser.h

--- code/parser.h
+++ code/parser.h
@@ -1,1 +1,7 @@
+/**
+ * @file parser.h 
+ * 
+ * Este é o ficheiro que contêm as declarações das funções do fichero parser.c.
+ * 
+ */
 void parse(char *line);
--- code/pushpop.h
+++ code/pushpop.h
@@ -1,5 +1,11 @@
-//declaraçao das funçoes e variaveis pra ser usadas em pushpop.c
+/**
+ * @file pushpop.h 
+ * 
+ * Este é o ficheiro que contêm as declarações das funções do ficheiro pushpop.c
+ * 
+ */
 extern int stack[10240]; 
 extern int conta;
 void push(int x);
 int pop();
+void print_stack (int v[],int N);

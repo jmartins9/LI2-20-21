9301a69b90fa27b749ebbd6f27d1ea84f5150ebe
JARMartins <joaoantonioredondo1994@gmail.com>
2021-04-05 20:06:20 +0100

    Implementei a nova stack com varios tipos de dados

--- code/pushpop.c
+++ code/pushpop.c
@@ -1,58 +1,56 @@
-/**
- * @file pushpop.c  
- * 
- * Este é o ficheiro que contêm as funções relativas à stack 
- * 
- */
 #include <stdio.h>
-#include "pushpop.h" 
+#include <stdlib.h>
 
-/*
- * 
- * Array stack: criamos a stack.
- * 
- */
-int stack[10240];
-/*
- * 
- * Variável que nos diz o tamanho da stack.
- * 
- */
-int conta = 0; 
+#include "pushpop.h"
 
-/** 
- * Esta é a função que faz push, ou seja, adiciona um valor ao topo da stack.
- */
-void push(int x) { 
-    if (conta == 10240) {                  //caso a stack esteja cheia dá print ao enunciado abaixo escrito, e não faz nada 
-        printf("Nao ha espaço na stack\n");
-        return;
-    }
-    stack[conta] = x;
-    conta++; 
-}
-/** 
- * Esta é a função que faz pop, ou seja, retira um valor do topo da stack.
- * 
- * @returns O valor "resto", o valor retirado da stack.
- */
-int pop() { 
-    if (conta == 0) { 
-        printf("Nada pra tirar da stack\n");
-        return 0;                           //isto é um erro, secalhar parar o programa em vez?
-    }
-    int resto = stack[conta-1];             //guarda na variável resto o valor que está no topo da stack 
-    conta--;                                //decresce à variável que guarda o tamanho da stack
-    return resto;
+int has_type(DATA elem, int mask) {
+  return (elem.type & mask) != 0;
 }
 
-/** 
- * Esta é a função que imprime a stack, imprime o resultado debaixo para o topo da stack 
- */
-void print_stack (int v[],int N) {
-    int i;
-    for (i=0;i<N;i++) {
-        printf("%d",v[i]);
-    }
-    printf("\n");
+STACK *create_stack() {
+  STACK *s = (STACK *) calloc(1, sizeof(STACK));
+  s->size = 100;
+  s->stack = (DATA *) calloc(s->size, sizeof(DATA));
+  return s;
 }
+
+void push(STACK *s, DATA elem) {
+  if(s->size == s->n_elems) {
+    s->size += 100;
+    s->stack = (DATA *) realloc(s->stack, s->size * sizeof(DATA));
+  }
+  s->stack[s->n_elems] = elem;
+  s->n_elems++;
+}
+
+DATA pop(STACK *s) {
+  s->n_elems--;
+  return s->stack[s->n_elems];
+}
+
+DATA top(STACK *s) {
+  return s->stack[s->n_elems - 1];
+}
+
+int is_empty(STACK *s) {
+  return s->n_elems == 0;
+}
+
+void print_stack(STACK *s) {
+  for(int K = 0; K < s->n_elems; K++) {
+    DATA elem = s->stack[K];
+    TYPE type = elem.type;
+    switch(type) {
+      case LONG:
+        printf("%ld", elem.elems.LONG); break;
+      case DOUBLE:
+        printf("%g", elem.elems.DOUBLE); break;
+      case CHAR:
+        printf("%c", elem.elems.CHAR); break;
+      case STRING:
+        printf("%s", elem.elems.STRING); break;
+    }
+  }
+  printf("\n");                               
+}
+
--- code/pushpop.h
+++ code/pushpop.h
@@ -4,8 +4,39 @@
  * Este é o ficheiro que contêm as declarações das funções do ficheiro pushpop.c
  * 
  */
-extern int stack[10240]; 
-extern int conta;
-void push(int x);
-int pop();
-void print_stack (int v[],int N);
+#ifndef ___STACK_H___
+#define ___STACK_H___
+
+#include <assert.h>
+
+typedef enum {LONG = 1, DOUBLE = 2, CHAR = 4, STRING = 8} TYPE;
+
+#define INTEGER  (LONG | CHAR)
+#define NUMBER   (INTEGER | DOUBLE)
+
+
+typedef struct data {
+  TYPE   type;
+  union {
+  long   LONG;
+  double DOUBLE;
+  char   CHAR;
+  char*  STRING;
+  } elems;
+} DATA;
+
+typedef struct stack {
+  DATA *stack;
+  int size;
+  int n_elems;
+} STACK;
+
+int has_type(DATA elem, int mask);
+STACK *create_stack();
+void push(STACK *s, DATA elem);
+DATA pop(STACK *s);
+DATA top(STACK *s);
+int is_empty(STACK *s);
+void print_stack(STACK *s);
+
+#endif

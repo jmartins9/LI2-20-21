e3dd91175ffce71624d1b999a6c4477dd5f5c596
JARMartins <joaoantonioredondo1994@gmail.com>
2021-04-16 16:07:53 +0100

    ficheiro header do stackoperations.c

---
+++ code/stackoperations.h
@@ -0,0 +1,55 @@
+/**
+ * @file stackoperations.h
+ *
+ * Este é o ficheiro que contém o header de todas as funções relacionadas com as operções sobre a stack. 
+ * 
+ */
+#ifndef ___STACKOPERATIONS_H___
+#define ___STACKOPERATIONS_H___
+
+#include <assert.h>
+#include "pushpop.h"
+
+/**
+ *
+ * Esta é o header daa função que decide qual das operações sobre a stack deve ser executada dependendo da instrução dada.
+ * 
+ */
+void stackoperations (char *token,STACK *s);
+
+/**
+ *
+ * Este é o header da função que executa o comando ;.
+ * 
+ */
+void retiratop (STACK *S);
+
+/**
+ *
+ * Este é o header da função que executa o comando \.
+ * 
+ */
+void changetop2 (STACK *s);
+
+/**
+ *
+ * Este é o header da função que executa o comando _.
+ * 
+ */
+void duplicatop (STACK *s);
+
+/**
+ *
+ * Este é o header da função que executa o comando @.
+ * 
+ */
+void changetop3 (STACK *s);
+
+/**
+ *
+ * Este é o header da função que executa o comando n $.
+ * 
+ */
+void copynelem (STACK *s);
+
+#endif

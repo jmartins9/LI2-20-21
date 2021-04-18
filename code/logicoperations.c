14fc586191425a3a94d74da25cb9c6e4585e3a85
JARMartins <joaoantonioredondo1994@gmail.com>
2021-04-18 16:38:22 +0100

    Acrescentei ficheiros para as operaçoes logicas

---
+++ code/logicoperations.c
@@ -0,0 +1,8 @@
+/**
+ * @file stackoperations.c 
+ *
+ * Este é o ficheiro que contém todas as funções relacionadas com as operações lógitas. 
+ */
+#include <stdio.h>
+#include <string.h>
+#include "pushpop.h"
---
+++ code/logicoperations.h
@@ -0,0 +1,14 @@
+/**
+ * @file stackoperations.h
+ *
+ * Este é o ficheiro que contém o header de todas as funções relacionadas com as operações lógicas. 
+ * 
+ */
+#ifndef ___LOGICOPERATIONS_H___
+#define ___LOGICOPERATIONS_H___
+
+#include <assert.h>
+#include "pushpop.h"
+
+
+#endif

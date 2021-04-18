d1d320035097ac1731c0b6b75e7f4f2baa372cb9
JARMartins <joaoantonioredondo1994@gmail.com>
2021-04-18 16:43:08 +0100

    documentaçao do parser.h

--- code/parser.h
+++ code/parser.h
@@ -4,8 +4,31 @@
  * Este é o ficheiro que contêm as declarações das funções do fichero parser.c.
  * 
  */
+#ifndef ___PARSER_H___
+#define ___PARSER_H___
+
 #include "pushpop.h"
+
+/**
+ *
+ * Esta é o header da função que converte uma linha em tipo de dados STRING.
+ * 
+ */
 void leituradatas (char *line,STACK *s);
+
+/**
+ *
+ * Esta é o header da função que guarda a linha de comandos dada numa string.
+ * 
+ */
 void preenche (char *line,char v[]);
+
+/**
+ *
+ * Esta é o header da função responsável pelo parser da linha de comandos dada.
+ * 
+ */
 void parse(char *line,STACK *s);
 
+
+#endif

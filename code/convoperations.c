408b343327040f1c8ab046e0f113d8742f171ebe
JARMartins <joaoantonioredondo1994@gmail.com>
2021-04-22 13:41:19 +0100

    acrescentei ficheiros para as operaçoes de conversao

---
+++ code/convoperations.c
@@ -0,0 +1,6 @@
+/**
+ * @file convoperations.c
+ *
+ * Este é o ficheiro que contém todas as funções relacionadas com as conversões. 
+ * 
+ */
---
+++ code/convoperations.h
@@ -0,0 +1,13 @@
+/**
+ * @file convoperations.h
+ *
+ * Este é o ficheiro que contém o header de todas as funções relacionadas com as conversões. 
+ * 
+ */
+#ifndef ___CONVOPERATIONS_H___
+#define ___CONVOPERATIONS_H___
+
+#include "pushpop.h"
+
+
+#endif

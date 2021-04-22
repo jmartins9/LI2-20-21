90a66954628119e3cb33e6b43a8c7cf35c500089
JARMartins <joaoantonioredondo1994@gmail.com>
2021-04-22 13:54:58 +0100

    adicionei includes

--- code/convoperations.c
+++ code/convoperations.c
@@ -4,3 +4,11 @@
  * Este é o ficheiro que contém todas as funções relacionadas com as conversões. 
  * 
  */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "variables.h"
+#include "pushpop.h"
+
+
+

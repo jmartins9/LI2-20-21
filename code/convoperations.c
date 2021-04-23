3632880311bdf0c2e5fe7dc89caa8b3c6c23dc6d
Afonso1611 <afinhaamorim@gmail.com>
2021-04-23 09:08:33 +0100

    convoperations

--- code/convoperations.c
+++ code/convoperations.c
@@ -10,5 +10,42 @@
 #include "variables.h"
 #include "pushpop.h"
 
+void convertLong (STACK *s) {
+    DATA data;
+    DATA p1 = pop(s);
+    if (what_type (p1)==LONG) {long conv = p1.elems.LONG;make_datas(data,LONG,conv);}
+    if (what_type (p1)==DOUBLE) {long conv = p1.elems.DOUBLE;make_datas(data,LONG,conv);}
+    if (what_type (p1)==CHAR) {long conv = p1.elems.CHAR;make_datas(data,LONG,conv);}
+    if (what_type (p1)==STRING) {long conv = strtol(p1.elems.STRING,NULL,10);make_datas(data,LONG,conv);}
+    push (s,data);
+}
+
+void convertFloat (STACK *s) {
+    DATA data;
+    DATA p1 = pop(s);
+    if (what_type (p1)==LONG) {float conv = p1.elems.LONG;make_datas(data,DOUBLE,conv);}
+    if (what_type (p1)==DOUBLE) {float conv = p1.elems.DOUBLE;make_datas(data,DOUBLE,conv);}
+    if (what_type (p1)==CHAR) {float conv = p1.elems.CHAR;make_datas(data,DOUBLE,conv);}
+    if (what_type (p1)==STRING) {float conv = strtod(p1.elems.STRING,NULL);make_datas(data,DOUBLE,conv);}
+    push (s,data);
+}
+
+void convertChar (STACK *s) {
+    DATA p1 = pop(s);
+    DATA data;
+    if (what_type (p1)==LONG) {char conv = p1.elems.LONG;make_datas(data,CHAR,conv);}
+    if (what_type (p1)==DOUBLE) {char conv = p1.elems.DOUBLE;make_datas(data,CHAR,conv);}
+    if (what_type (p1)==CHAR) {char conv = p1.elems.CHAR;make_datas(data,CHAR,conv);}
+    push(s,data);
+}
+
+void convoperations (char *token,STACK *s) {
+    switch (*token) {
+        case ('f'): somar(S);break;
+        case ('c'): subtrair(s);break;
+        case ('i'): multiplicar(s);break;
+    }
+}
+
 
 

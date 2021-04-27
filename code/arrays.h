25ffdaac9389270e0c1a944913d9f612720a7796
JARMartins <joaoantonioredondo1994@gmail.com>
2021-04-27 19:13:55 +0100

    ficheiro header dos arrays

---
+++ code/arrays.h
@@ -0,0 +1,18 @@
+/**
+ * @file arrays.h
+ *
+ * Este é o ficheiro que contém o header de todas as funções relacionadas com os arrays. 
+ * 
+ */
+#ifndef ___ARRAYS_H___
+#define ___ARRAYS_H___
+
+/**
+ * 
+ * Header da função que divide uma linha em um tokem e o resto da mesma. 
+ * 
+ */
+char *get_token(char *line, char **rest) ;
+
+
+#endif

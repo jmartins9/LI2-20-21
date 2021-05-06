97469ec2bae059ff67433f1cdee74e0c752ac71c
JARMartins <joaoantonioredondo1994@gmail.com>
2021-05-06 14:53:59 +0100

    ficheiros para blocos

---
+++ code/blocos.c
@@ -0,0 +1,14 @@
+/**
+ * @file blocos.c
+ *
+ * Este é o ficheiro que contém todas as funções relacionadas com os blocos. 
+ * 
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "pushpop.h"
+#include "variables.h"
+#include "parser.h"
+#include "matoperations.h"
+
---
+++ code/blocos.h
@@ -0,0 +1,13 @@
+/**
+ * @file blocos.h
+ *
+ * Este é o ficheiro que contém o header de todas as funções relacionadas com os arrays. 
+ * 
+ */
+#ifndef ___BLOCOS_H___
+#define ___BLOCOS_H___
+
+#include "variables.h"
+
+
+#endif
